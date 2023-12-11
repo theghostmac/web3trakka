8b630a37c382520c1ecc6b23c02a69d1fad5785d
theghostmac <theghostmac@gmail.com>
2023-12-11 22:38:48 +0100

    [feature] implemented client for kucoin

--- external/kucoin/client.go
+++ external/kucoin/client.go
@@ -3,17 +3,26 @@ package kucoin
 import (
 	"github.com/Kucoin/kucoin-go-sdk"
 	"github.com/theghostmac/web3trakka/external/crypto"
+	"os"
 )
 
 type KucoinClient struct {
 	Client *kucoin.ApiService
 }
 
-type Opts struct {
-}
+// NewKucoinClient creates a new Kucoin client.
+func NewKucoinClient() *KucoinClient {
+	apiKey := os.Getenv("KUCOIN_API_KEY")
+	baseURL := os.Getenv("KUCOIN_PRODUCTION_URL")
+	apiSecret := os.Getenv("KUCOIN_API_SECRET")
 
-func NewKucoinClient(key, secret, passphrase string) *KucoinClient {
-	client := kucoin.NewApiService()
+	client := kucoin.NewApiService(
+		kucoin.ApiKeyOption(apiKey),
+		kucoin.ApiBaseURIOption(baseURL),
+		kucoin.ApiSecretOption(apiSecret),
+		kucoin.ApiKeyVersionOption(kucoin.ApiKeyVersionV2),
+	)
+
 	return &KucoinClient{Client: client}
 }
 
@@ -22,6 +31,4 @@ func (kc *KucoinClient) GetSymbolDetails(cryptoPair string) (*crypto.SymbolDetai
 	return nil, nil
 }
 
-func (kc *KucoinClient) ExecuteTrade() {
-
-}
+func (kc *KucoinClient) ExecuteTrade() {}
